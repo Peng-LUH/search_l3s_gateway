c28ada77e8139c24161785ef292ead0483d1756d
Peng-LUH <ethaniel.unihannover@gmail.com>
2023-03-22 17:08:47 +0100

    update 2023-03-22-17:08

--- src/search_l3s_gateway/util/mls_api.py
+++ src/search_l3s_gateway/util/mls_api.py
@@ -9,6 +9,38 @@ MLS_LOGIN_PAYLOAD = {
   "grant_type": os.getenv("MLS_GRANT_TYPE"),
 }
 
+class MLS_CONTENT_TYPE(object):
+    DIRECTORIIES = "directories"
+    DOCUMENTS = "documents"
+    EXTERNAL_ILIAS_COURSES = "external-ilias-courses"
+    FEEDBACK = "feedback"
+    FORMS = "forms"
+    FORM_SET = "form-sets"
+    GROUPS = "groups"
+    GROUP_TASK_TODOS = "group-task-todos"
+    JOB = "jobs"
+    ORGANIZATION = "organizations"
+    PROJECT = "projects"
+    PROJECT_TASK = "project-tasks"
+    PROJECT_TODO = "project-todos"
+    SKILL = "skills"
+    TAG = "tags"
+    
+    TASK = "tasks"
+    TASK_SKILL = "task-skills"
+    TASK_FILE = "task-files"
+    TASK_SET = "task-sets"
+    TASK_SET_TRANSLATION = "task-set-translations"
+    
+    TASK_STEP = "task-steps"
+    TASK_STEP_FILE = "task-step-files"
+    TASK_STEP_CATEGORY = "task-step-categories"
+    
+    TASK_TODO = "task-todos"
+    TASK_TODO_INFO = "task-todo_infos"
+    TASK_TODO_FILE = "task-todo-files"
+    
+    USER = "users"
 
 class MLSConnection(object):
     def get_login_response(self, login_server_url, realm, login_payload):
@@ -19,15 +51,16 @@ class MLSConnection(object):
                 "Content-Type": "application/x-www-form-urlencoded",
                 }
             )
-        return login_response.json()
+        return login_response
     
     def get_access_token(self, login_response): 
         return login_response.json()["access_token"]
     
-    def get_auth_header(self, access_token):
+    def get_auth_header(self, access_token, accept_header="application/ld+json"):
         auth_header =  {
             "Authorization": "Bearer " + access_token,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "accept": accept_header,
             }
         return auth_header
     
