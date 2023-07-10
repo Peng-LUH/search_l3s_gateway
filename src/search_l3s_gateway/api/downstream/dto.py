1457b235f817fc1a1c940b637fe78a2baa81dacd
Peng-LUH <ethaniel.unihannover@gmail.com>
2023-07-10 15:09:15 +0200

    update

---
+++ src/search_l3s_gateway/api/downstream/dto.py
@@ -0,0 +1,23 @@
+from flask_restx import Model, fields
+
+
+todo = Model('Todo', {
+    'id': fields.Integer(readonly=True, description='The task unique identifier'),
+    'task': fields.String(required=True, description='The task details')
+})
+
+search_srv_request_model = Model('Search_Srv_Request', {
+    "uid": fields.String(description="user ID", default=None),
+    "cid": fields.String(description="company ID", default=None),
+    "query": fields.String(required=True, default="Elektrotechnik 1 Versuch 8: Wirkleistung von Wechselspannungen; Wirkleistung der Sinuswechselspannung in der praktischen \u00dcbung"),
+    "language_model": fields.String(default="bert-base-german-cased"),
+    "index_method": fields.String(default="flat-l2"),
+    "dataset_name": fields.String(default="mls-tasks-full"),
+    "nr_result": fields.Integer(min=1, default=10)
+})
+
+recsys_srv_request_model = Model('Recsys_Srv_Request', {
+    "uid": fields.String(description="user ID", default=None),
+    "cid": fields.String(description="company ID", default=None),
+    "catetory": fields.String(description="recsys category", default=None)
+})
--- src/search_l3s_gateway/api/downstream/endpoints.py
+++ src/search_l3s_gateway/api/downstream/endpoints.py
@@ -1,50 +1,54 @@
 # from flask import request
 from flask_restx import Namespace, Resource, fields
 from http import HTTPStatus
+import requests, json
 
 # from search_l3s_gateway.api import api
+from .dto import (
+    search_srv_request_model,
+    recsys_srv_request_model
+    )
 
 ns_downstream = Namespace("downstream", validate=True)
 
-todo = ns_downstream.model('Todo', {
-    'id': fields.Integer(readonly=True, description='The task unique identifier'),
-    'task': fields.String(required=True, description='The task details')
-})
+# register dto models
+ns_downstream.models[search_srv_request_model.name] = search_srv_request_model
+ns_downstream.models[recsys_srv_request_model.name] = recsys_srv_request_model
 
 
-@ns_downstream.route("/test_downstream", endpoint="test_downstream")
-class TestUpstream(Resource):
-    @ns_downstream.response(int(HTTPStatus.CREATED), "successfully get.")
-    @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
-    @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
-    @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    def get(self):
-        return {"message": "get method from test downstream endpoint."}, HTTPStatus.OK
+# @ns_downstream.route("/test_downstream", endpoint="test_downstream")
+# class TestUpstream(Resource):
+#     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully get.")
+#     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
+#     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
+#     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
+#     def get(self):
+#         return {"message": "get method from test downstream endpoint."}, HTTPStatus.OK
 
-    @ns_downstream.response(int(HTTPStatus.CREATED), "successfully created.")
-    @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
-    @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
-    @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    @ns_downstream.expect(todo)
-    def post(self):
-        data = ns_downstream.payload
-        return data, HTTPStatus.CREATED
+#     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully created.")
+#     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
+#     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
+#     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
+#     @ns_downstream.expect(todo)
+#     def post(self):
+#         data = ns_downstream.payload
+#         return data, HTTPStatus.CREATED
 
-    @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
-    @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
-    @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
-    @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    @ns_downstream.expect(todo)
-    def put(self):
-        data = ns_downstream.payload
-        return data, HTTPStatus.ACCEPTED
+#     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
+#     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
+#     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
+#     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
+#     @ns_downstream.expect(todo)
+#     def put(self):
+#         data = ns_downstream.payload
+#         return data, HTTPStatus.ACCEPTED
 
-    @ns_downstream.response(int(HTTPStatus.CREATED), "successfully deleted.")
-    @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
-    @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
-    @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    def delete(self):
-        return {"message": "delete method downstream"}, HTTPStatus.OK
+#     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully deleted.")
+#     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
+#     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
+#     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
+#     def delete(self):
+#         return {"message": "delete method downstream"}, HTTPStatus.OK
 
 
 ### Connection: search services
@@ -54,26 +58,41 @@ class SearchService(Resource):
     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    @ns_downstream.expect(todo)
+    @ns_downstream.expect(search_srv_request_model)
     def post(self):
+        url = 'http://127.0.0.1:5001/api/v1/searcher/dense-retrieval'
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        data = ns_downstream.payload
+        response = requests.post(url, headers=headers, data=json.dumps(data))
         
-        pass
+        return response.json()
     
 
 ### Connection: recommendation service
-@ns_downstream.route('/recommendation-service', endpoint="rec_service")
+@ns_downstream.route('/recsys-service', endpoint="recsys_service")
 class RecommendationService(Resource):
     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    @ns_downstream.expect(todo)
+    @ns_downstream.expect(recsys_srv_request_model)
     def post(self):
-        pass
+        url = 'http://127.0.0.1:5002/api/v1/searcher/dense-retrieval'
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        data = ns_downstream.payload
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        
+        return response.json()
     
 
 ### Connection: question generation service
-@ns_downstream.route('/question-generation-service', endpoint="qgen_service")
+@ns_downstream.route('/ai-meta-service', endpoint="ai_meta_service")
 class QGenService(Resource):
     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
@@ -84,15 +103,15 @@ class QGenService(Resource):
         pass
     
 
-### Connection: summary service
-@ns_downstream.route('/summary-service', endpoint="summary_service")
-class SummaryService(Resource):
-    @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
-    @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
-    @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
-    @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
-    @ns_downstream.expect(todo)
-    def post(self):
-        pass
+# ### Connection: summary service
+# @ns_downstream.route('/summary-service', endpoint="summary_service")
+# class SummaryService(Resource):
+#     @ns_downstream.response(int(HTTPStatus.CREATED), "successfully changed.")
+#     @ns_downstream.response(int(HTTPStatus.CONFLICT), "exits conflict.")
+#     @ns_downstream.response(int(HTTPStatus.BAD_REQUEST), "validation error.")
+#     @ns_downstream.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "internal server error.")
+#     @ns_downstream.expect(todo)
+#     def post(self):
+#         pass
     
 
